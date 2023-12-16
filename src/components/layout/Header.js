5caf15a94999a180e4166f833ad674584264a3c9
Akhil Augustin <akhiljw46@gmail.com>
2023-12-16 20:49:53 +0530

    Changed logo and mobile view

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,10 @@
-import { ReactComponent as Logo } from '../../images/logo.svg';
+import Logo from '../../images/kathy.png';
 import classes from './Header.module.css';
 
 const Header = () => {
   return (
     <header className={classes.header}>
-      <Logo className={classes.logo} />
+      <img src={Logo} alt="logo" className={classes.logo} />
     </header>
   );
 };
